d99d52bc4488aaaf30dc6fb43aefec052f611951
Ruslan Normuminov <RilPax@yandex.ru>
2024-10-07 18:47:54 +1000

    commit-1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,18 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardContainer = document.querySelector('.places__list');
+const cardAdd = (initialCards) => {
+  for(let i = 0; i < initialCards.length; i++) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    card.querySelector('.card__title').textContent = initialCards[i].name;
+    card.querySelector('.card__image').src = initialCards[i].link;
+    const deleteButton = card.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+      card.remove();
+    });
+    cardContainer.append(card);
+  }
+}
+cardAdd(initialCards);
